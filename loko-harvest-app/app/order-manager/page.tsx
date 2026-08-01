2bdc30f3e041ff8a8079ae13d6b44ce0469c2f01
katsomar <katsomar60@gmail.com>
2026-08-01 16:40:03 +0300

    fix: import Link from next/link in order manager page

--- loko-harvest-app/app/order-manager/page.tsx
+++ loko-harvest-app/app/order-manager/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import dynamic from "next/dynamic";
 import { 
